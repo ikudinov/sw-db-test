1ff914ade4a8f610783a9a05ef38c4168aab046f
Ivan Kudinov <ikudinov@ccsteam.ru>
2021-11-28 15:43:31 +0300

    Build using relative path

---
+++ docs/precache-manifest.2263d28329c1dd338d52f45518873444.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8feff826f899da007be6efea3f3df50",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f24da5e9075d2f762036",
+    "url": "./static/css/main.532b7f5a.chunk.css"
+  },
+  {
+    "revision": "74fc65ed53fcf5ba0f11",
+    "url": "./static/js/2.c139a930.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.c139a930.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f24da5e9075d2f762036",
+    "url": "./static/js/main.68f06a19.chunk.js"
+  },
+  {
+    "revision": "ebb3c6fa8120e08c416a",
+    "url": "./static/js/runtime-main.cfbbfe63.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.61904f76059dda024d6781745de1359a.js"
+  "./precache-manifest.2263d28329c1dd338d52f45518873444.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
